66423d13fb7064f704d63cfa06a59eb2fa717da3
Kat <144715725+katkowska@users.noreply.github.com>
2024-01-30 23:12:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -25,21 +25,46 @@ const userRoutes = require('./routes/users');
 const venueRoutes = require('./routes/venues');
 const reviewRoutes = require('./routes/reviews');
 
+const MongoStore = require('connect-mongo');
+
 const mapboxgl = require('mapbox-gl');
 
-
-mongoose.connect('mongodb://localhost:27017/gigaunty', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false
-});
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/gigaunty";
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Connection error:"));
 db.once("open", () => {
     console.log("Database connected.");
 });
+
+// // HERE'S THE MONGO Data Base CONNECTION
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const dbUrl = process.env.DB_URL;
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(dbUrl, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
 
 const app = express();
 
@@ -51,9 +76,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+    secret: 'thisshouldbeabettersecret!'
+    }
+    });
+
+store.on("error", function(e) {
+   console.log("Session store error!", e)
+});
+
 const sessionConfig = {
     name: 'session',
-    secret: 'something',
+    secret: 'thisshouldbeabettersecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -112,9 +149,9 @@ app.use(
 );
 
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(mongoSanitize({
-//     replaceWith: '_'
-// }));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
